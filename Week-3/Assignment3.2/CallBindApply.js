83c1ee8b7b8242e2ff2b9ba5325765a8dea1e292
Shabnam14 <shabnamhannan786@gmail.com>
2022-07-14 15:26:35 +0530

    Assignment on call, bind, apply

---
+++ Week-3/Assignment3.2/CallBindApply.js
@@ -0,0 +1,49 @@
+// Call function
+function industry(type, lang){
+    this.type = type;
+    this.lang = lang;
+}
+function movie(movieName){
+    industry.call(this, "Kollywood", "Kannada");
+    this.movieName = movieName;
+    console.log("Movie details are ", this);
+}
+function series(seriesName){
+    industry.call(this, "Hollywood", "English");
+    this.seriesName = seriesName;
+    console.log("Series details are ", this);
+}
+const movieCall = new movie("KGF");
+const seriesCall = new series("Stranger things");
+
+// Apply function
+function industry(type, lang){
+    this.type = type;
+    this.lang = lang;
+}
+function movie(movieName){
+    industry.apply(this, ["Kollywood", "Kannada"]);
+    this.movieName = movieName;
+    console.log("Movie details are ", this);
+}
+function series(seriesName){
+    industry.apply(this, new Array("Hollywood", "English"));
+    this.seriesName = seriesName;
+    console.log("Series details are ", this);
+}
+const movieApply = new movie("KGF");
+const seriesApply = new series("Stranger things");
+
+// Bind Fucntion
+const myfirst = {
+    series: "Stranger things",
+    seriesdet: function series(){
+    console.log("Series details are ", this);
+    },
+};
+const SeriesObj = {
+    series: "Game of Thrones",
+};
+let ser = myfirst.seriesdet.bind(SeriesObj);
+ser();
+console.log(myfirst);
